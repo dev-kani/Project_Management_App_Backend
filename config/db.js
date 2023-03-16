2df8824094622bf6e2ee7faaca39bd3fa3ed3aff
Kanishka Samarathunga <kanishkakcs@gmail.com>
2023-03-16 06:48:55 +0530

    mongo async removed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI)
+const connectDB = () => {
+  mongoose.connect(process.env.MONGO_URI)
   console.log(`Connected to MongoDB `.brightBlue.bold)
 };
 
